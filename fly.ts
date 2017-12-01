1b7a2ec515ffe8d20a94677d4f5c6a7bc70bcff9
lethek <michael@lethek.net>
2017-12-01 09:46:35 +0000

    Update fly.ts

--- fly.ts
+++ fly.ts
@@ -1,5 +1,6 @@
 const magik = magikcraft.io;
 
 function fly() {
-	magik.volare(60000);
+	magik.volare(10000);
+   // magik.setTimeout(() => magik.dixit("Volare spell is about to run out!!"), 50000);
 }
