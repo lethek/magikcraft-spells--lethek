47fed26db0d43013ac8438f8eaede5cdf2f11919
lethek <michael@lethek.net>
2017-12-01 09:14:32 +0000

    Update boom.ts

--- boom.ts
+++ boom.ts
@@ -1,5 +1,11 @@
 const magik = magikcraft.io;
 
 function boom() {
-    magik.shakti();
+    let strikes = 0;
+    var timer = magik.setInterval(() => {
+        magik.shakti();
+        if (strikes++ > 10) {
+            magik.clearInterval(timer);
+        }
+    }, 1000);
 }
